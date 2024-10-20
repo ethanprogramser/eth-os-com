8a913dcbdf73ec83b2fb8055e38574ecbe4d32d7
Nyvyme <mixagame228@gmail.com>
2024-10-20 06:56:03 +0500

    Implemented kernel mode `ctype`.

---
+++ ethcore/klib/kctype.h
@@ -0,0 +1,20 @@
+#ifndef __K_KLIB_CTYPE_H__
+#define __K_KLIB_CTYPE_H__
+
+static int __kisalnum(char c);
+static int __kisalpha(char c);
+static int __kisblank(char c);
+static int __kiscntrl(char c);
+static int __kisdigit(char c);
+static int __kisgraph(char c);
+static int __kislower(char c);
+static int __kisprint(char c);
+static int __kispunct(char c);
+static int __kisspace(char c);
+static int __kisupper(char c);
+static int __kisxdigit(char c);
+
+static char __ktoupper(char c);
+static char __ktolower(char c);
+
+#endif // __K_KLIB_CTYPE_H__
