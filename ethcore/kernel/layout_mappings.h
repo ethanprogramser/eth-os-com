06cda9e94170e1efa3fed9478f3ffcf5e029194d
Nyvyme <mixagame228@gmail.com>
2024-10-22 08:15:13 +0500

    Added list of scancodes, keycodes, and layouts.
    For future use in new keyboard driver.

---
+++ ethcore/kernel/layout_mappings.h
@@ -0,0 +1,10 @@
+#ifndef __K_LAYOUT_MAPPINGS_H__
+#define __K_LAYOUT_MAPPINGS_H__
+
+enum LayoutMapping
+{
+  LAYOUT_MAPPING_QWERTY = 0,
+  LAYOUT_MAPPING_DVORAK = 1,
+};
+
+#endif // __K_LAYOUT_MAPPINGS_H__
