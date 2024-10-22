451b1f080723865d8ad3ce6070f7d78ba6216a4c
ethanprogramser <168668562+ethanprogramser@users.noreply.github.com>
2024-10-22 17:04:06 +0000

    Update kmemory.h

--- ethcore/klib/kmemory.h
+++ ethcore/klib/kmemory.h
@@ -6,5 +6,6 @@
 
 void *__kmemcpy (void *dst, const void *src, size_t n);
 void *__kmemset (void *blk, int c, size_t n);
+uint32_t ethoc(uint32_t size, int align, uint32_t *paddr);
 
 #endif // __K_KLIB_MEMORY_H__
