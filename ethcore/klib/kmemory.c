5e55578df86500d8a59314934d2add06903c251f
ethanprogramser <168668562+ethanprogramser@users.noreply.github.com>
2024-10-22 17:04:52 +0000

    Update kmemory.c

--- ethcore/klib/kmemory.c
+++ ethcore/klib/kmemory.c
@@ -1,5 +1,6 @@
 #include "klib/kmemory.h"
 
+uint32_t mem_start = 0x100000
 void *
 __kmemcpy (void *dst, const void *src, size_t n)
 {
@@ -14,4 +15,17 @@ __kmemset (void *blk, int c, size_t n)
   for (size_t i = 0; i < n; i++)
     ((uint8_t *)blk)[i] = c;
   return blk;
+}
+
+uint32_t ethoc(uint32_t size, int align, uint32_t *paddr) {
+      if(align == 1 && (mem_start & 0xFFFFF000)) {
+          mem_start &= 0xFFFFF000;
+          mem_start += 0x100000;
+      }
+
+      if(paddr) *paddr = mem_start;
+
+      uint32_t ret = mem_start;
+      mem_start+=size;
+      return ret;
 }
