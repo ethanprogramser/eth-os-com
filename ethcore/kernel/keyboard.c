1dd1368c3bb1d732617497dbe58783cada775ba3
Nyvyme <mixagame228@gmail.com>
2024-11-01 02:04:17 +0500

    Removed not used `printable_keycodes` array from `ethcore/kernel/keyboard.c`.

--- ethcore/kernel/keyboard.c
+++ ethcore/kernel/keyboard.c
@@ -20,62 +20,6 @@ static struct KeyboardState keyboard_state = { 0 };
 
 static void __keyboard_add_to_keycode_list (enum Keycode keycode);
 
-static bool printable_keycodes[0xFF] = {
-  // 0x00 (reserved)
-  0,
-
-  // 0x01-0x04
-  1, 1, 1, 0,
-
-  // 0x05-0x0F
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-
-  // 0x10-0x19
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-
-  // 0x1A-0x1F (reserved)
-  0, 0, 0, 0, 0, 0,
-
-  // 0x20-0x39
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-
-  // 0x3A-0x3F (reserved)
-  0, 0, 0, 0, 0, 0,
-
-  // 0x40-0x42
-  0, 1, 0,
-
-  // 0x43-0x4F (reserved)
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-
-  // 0x50-0x5B
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-
-  // 0x5C
-  0,
-
-  // 0x5D-0x5F (reserved)
-  0, 0, 0,
-
-  // 0x60-0x63
-  0, 0, 0, 0,
-
-  // 0x64-0x6F (reserved)
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-
-  // 0x70-0x71
-  0, 0,
-
-  // 0x72-0x75
-  0, 0, 0, 0,
-
-  // 0x76-0x7F (reserved)
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-
-  // 0x80-0x87
-  0, 0, 0, 0, 0, 0, 0, 0
-};
-
 void
 keyboard_init (void)
 {
