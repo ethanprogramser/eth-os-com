65565a19beedca9307f3a4b8706abd39889f00b3
Nyvyme <mixagame228@gmail.com>
2024-11-03 10:46:14 +0500

    Implemented `__kstrcmp`.

--- ethcore/klib/kstring.c
+++ ethcore/klib/kstring.c
@@ -22,3 +22,12 @@ __kstrncpy (char *dst, const char *src, int n)
   for (; *src && n > 0; *dst++ = *src++, --n)
     ;
 }
+
+int
+__kstrcmp (const char *str1, const char *str2)
+{
+  while (*str1 == *str2++)
+    if (*str1++ == 0)
+      return 0;
+  return *(const unsigned char *)str1 - *(const unsigned char *)str2;
+}
--- ethcore/klib/kstring.h
+++ ethcore/klib/kstring.h
@@ -4,5 +4,7 @@
 int __kstrlen (const char *str);
 void __kstrcpy (char *dst, const char *src);
 void __kstrncpy (char *dst, const char *src, int n);
+int __kstrcmp (const char *str1, const char *str2);
+#define __kstreq(str1, str2) (__kstrcmp ((str1), (str2)) == 0)
 
 #endif // __K_KLIB_STRING_H__
