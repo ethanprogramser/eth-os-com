61a72c414a01504e195ef41ddd4474400f4cc4f7
senhas-rgb <senhasdiwan@gmail.com>
2024-11-06 17:05:43 +0530

    Memory management improvements

--- ethcore/klib/kmemory.c
+++ ethcore/klib/kmemory.c
@@ -5,16 +5,18 @@ uint32_t mem_start = 0x100000;
 uint32_t
 ethoc (uint32_t size, int align, uint32_t *paddr)
 {
-  if (align == 1 && (mem_start & 0xFFFFF000))
-  {
-    mem_start &= 0xFFFFF000;
-    mem_start += 0x100000;
+  if (align > 1) {
+    mem_start = (mem_start + align - 1) & ~(align - 1);
+  }
+
+  if (mem_start + size < mem_start) {
+    return 0; // Error: overflow
   }
 
   if (paddr)
     *paddr = mem_start;
 
-  uint32_t ret = mem_start;
+  uint32_t allocated = mem_start;
   mem_start += size;
-  return ret;
+  return allocated;
 }
