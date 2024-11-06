ff4ddba10f2a08b085a4fd72de34c1ae807a1d32
Nyvyme <mixagame228@gmail.com>
2024-11-06 20:48:21 +0500

    Added `-pedantic` flag for kernel cflags.

--- ethcore/ethcore.mk
+++ ethcore/ethcore.mk
@@ -1,3 +1,3 @@
 export ASMFLAGS=-f elf32
-export CFLAGS=-m32 -fno-stack-protector -fno-builtin -nostdlib -ffreestanding
+export CFLAGS=-m32 -fno-stack-protector -fno-builtin -nostdlib -ffreestanding -pedantic
 export LDFLAGS=-m elf_i386
