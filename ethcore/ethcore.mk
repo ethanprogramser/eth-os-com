5059249e828defc401b3de0fd203da424be54ec3
Nyvyme <mixagame228@gmail.com>
2024-11-11 20:46:04 +0500

    Now kernel makefile uses "GNU C 99" standard for `.c` files.

--- ethcore/ethcore.mk
+++ ethcore/ethcore.mk
@@ -1,3 +1,3 @@
 export ASMFLAGS=-f elf32
-export CFLAGS=-m32 -fno-stack-protector -fno-builtin -nostdlib -ffreestanding -pedantic -O0
+export CFLAGS=-m32 -fno-stack-protector -fno-builtin -nostdlib -ffreestanding -pedantic -O0 -std=gnu99
 export LDFLAGS=-m elf_i386
